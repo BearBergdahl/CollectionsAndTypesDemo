aea5da2ba4a9929cd710fded09801491ca5dc49d
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-10-16 15:07:24 +0200

    Added example code part1

--- CollectionsDemo/SimpleTypes.cs
+++ CollectionsDemo/SimpleTypes.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace CollectionsDemo
 {
-    internal class SimpleTypes
+    public class SimpleTypes
     {
+        public void DemoValueType()
+        {
+            bool isNumber= false;
+            string demoString;
+            int result;
+            bool isCopied;
+            string copyString;
+            int copyResult;
+            do
+            {
+                Console.WriteLine("Please enter integer value");
+                var inputRaw = Console.ReadLine();
+                Console.WriteLine($"Now what type is the input? {inputRaw.GetType}");
+                isNumber = int.TryParse(inputRaw, out result);
+                if (isNumber)
+                { 
+                    Console.WriteLine($"Int result parsed: {result}, of type: {result.GetType}"); 
+                }
+                Console.WriteLine("Lets play around with these");
+                demoString = inputRaw.ToString();
+
+            } while (!isNumber); 
+            
+        }
     }
 }
